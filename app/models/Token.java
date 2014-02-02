2364189ab5b4e5313805a6bd57929b16b07e7d04
xerophil <simon.beckstein@gmail.com>
2014-02-02 16:21:04 +0100

    -added user management stuff to the data model

---
+++ app/models/Token.java
@@ -0,0 +1,38 @@
+package models;
+
+import javax.persistence.OneToOne;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Unique token used for (initial) password reset and unlocking account
+ */
+@javax.persistence.Entity
+public class Token extends Entity {
+    public static Finder<UUID, Token> find = new Finder<UUID, Token>(UUID.class, Token.class);
+
+    /**
+     * Timestamp of creation
+     */
+    public Date timestamp;
+
+    /**
+     * The user the token belongs to; owned by the user
+     */
+    @OneToOne(mappedBy = "token", orphanRemoval = true)
+    public User user;
+
+    public Token() {
+        timestamp = new Date();
+    }
+
+    /**
+     * Overwritten to set the token null on the user-side before deleting it
+     */
+    @Override
+    public void delete() {
+        user.token = null;
+        user.update();
+        super.delete();
+    }
+}
